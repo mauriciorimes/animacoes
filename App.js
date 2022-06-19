af4e9d7a82454acdc612450e638233dd5021d7db
mauriciorimes <mauriciorimes@gmail.com>
2022-06-19 13:37:11 -0300

    projeto finalizado

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { Feather } from "react-native-vector-icons";
+import LottieView from "lottie-react-native";
 
 export default function App() {
+
+  let animation = React.createRef();
+
+  function startAnimation() {
+    animation.current.play();
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity onPress={startAnimation}>
+        <Feather name="play" size={80} color="green"/>
+      </TouchableOpacity>
+      <LottieView 
+        source={require("./assets/check.json")}
+        style={styles.icone}        
+        loop={false}
+        ref={animation}/> 
+
+      <Text style={styles.texto}>Enjoy and click anytime!</Text> 
+         
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  icone: {
+    width: 200,
+    height: 400
+  },
+  texto: {
+    fontSize: 20,
+    color: "green"
+  }
 });
---
+++ Readme.md
@@ -0,0 +1,3 @@
+Uma tela de app mobile que ao clicar no botao com icone de play,
+reproduz uma animacao de check.
+Uso de React Native.
